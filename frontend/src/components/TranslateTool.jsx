fcd45415ec1cf419e525766cd883a9ef70d6bd31
huyhd2334 <huyqame1356@gmail.com>
2025-11-06 11:28:34 +0700

    refine translate tool

--- frontend/src/components/TranslateTool.jsx
+++ frontend/src/components/TranslateTool.jsx
@@ -9,24 +9,14 @@ const TranslateTool = ({langue}) => {
         const word = document.getElementById("search").value.replaceAll(" ", "")
         const detail = await api.post("/finddetail",{word: word, langue: langue})
         if(detail.data.detail){
-            toast.loading(detail.data.detail["meaning"])
+            toast.info(detail.data.detail["meaning"], {duration: 10000})
         }else{toast.error("Out of dict range")}
     }
   return (
-    <div className="flex items-center justify-center bg-gray-50 w-[11px] h-[11px]">
-        <Button className="relative group flex flex-row w-16 h-12 rounded-4xl">
-            <div className='border-2 border-blue-500 rounded-4xl w-15 h-11 flex justify-center items-center'>
-                <span className="font-semibold text-blue-600 flex flex-row items-center justify-center space-x-3 border-2 border-blue bg-blue-500 text-black-800 rounded-4xl w-14 h-10"> 
-                    <Search/>
-                </span>
-            </div>
-            <input id="search" type="text" placeholder="Type single word here..." className="absolute left-0 top-full mt-2 w-0 opacity-0 
-                                                                        border border-blue-500 rounded-md px-2 py-1 
-                                                                        transition-all duration-500 ease-in-out 
-                                                                        group-hover:w-60 group-hover:opacity-100"/>
-            <Button onClick={()=>handleSearch()} className="absolute left-55 top-12 w-0 opacity-0 transition-all duration-500 ease-in-out bg-blue-500 group-hover:w-10 rounded-2xl group-hover:opacity-100 "><CircleArrowRight/></Button>
-        </Button>
-    </div>
+    <div className="flex items-center justify-center bg-gray-50 w-[11px] h-[11px] border-2 border-green-100 rounded-4xl">
+            <input id="search" type="text" placeholder="Type single word here..." className=" text-black w-100 h-7"/>
+            <Button onClick={()=>handleSearch()} className="w-7 h-7"><Search/></Button>
+            </div>    
   )
 }
 
