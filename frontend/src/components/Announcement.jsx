c794fe61565832e01a542a16a506ceabacdc7437
huyhd2334 <huyqame1356@gmail.com>
2025-12-14 14:05:48 +0700

    update / pic

--- frontend/src/components/Announcement.jsx
+++ frontend/src/components/Announcement.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Announcement = ({streak, userName, langue}) => {
   return (
       <div onClick={(e) => e.stopPropagation()} 
-           className={`relative inset-0 w-[900px] h-[500px] ${langue === "english" ? "bg-[url('english.png')]" : "bg-[url('chinese.png')]"} bg-cover bg-center rounded-3xl`}>
+           className={`relative inset-0 w-[900px] h-[500px] ${langue === "english" ? "bg-[url('/english.png')]" : "bg-[url('/chinese.png')]"} bg-cover bg-center rounded-3xl`}>
         <div className='absolute flex flex-row items-center  top-[57px] left-[630px] text-4xl font-bold text-white'> Streak: {streak}</div>
         <div className='absolute flex flex-row items-center  top-[215px] left-[180px] text-4xl font-bold text-white'> {userName} </div>
       </div>
