2089faaa6db1384cde6fdd3c296475c0175767b9
huyhd2334 <huyqame1356@gmail.com>
2025-12-17 14:44:07 +0700

    refine

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,9 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_CONNNECTIONSTRING);
 
-    console.log("Connect database successfully 🥳");
+    console.log("Connect database successfully");
   } catch (error) {
-    console.error("can not connect to database 🥵:", error);
+    console.error("can not connect to database:", error);
     process.exit(1); // exit with error
   }
 };
---
+++ backend/src/controler/LogicReviewVocab.js
@@ -0,0 +1,8 @@
+import UserVocabs from "../models/english/englishUserVocab.js";
+import ChineseUserVocabs from "../models/chinese/chineseUserVocab.js";
+
+export const spacedRepetition = async(req, res) => {
+       const {userAccount, langue} = req.body
+
+       
+ }
