6b9bd43f35a07655ddd7b9c7cfed8dd1a886e893
huyhd2334 <huyqame1356@gmail.com>
2025-12-18 17:11:18 +0700

    swicth servey to node js

--- backend/src/controler/LogicGenerateSurvey.js
+++ backend/src/controler/LogicGenerateSurvey.js
@@ -1,7 +1,7 @@
 import Dict from "../models/english/englishDictAllWord.js"
 
 const PREFIXES = ["un", "re", "pre", "mis", "dis", "in", "im", "non"];
-const SUFFIXES = ["tion", "sion", "ing", "ed", "able", "ible", "ment", "ness"];
+const SUFFIXES = ["tion", "sion", "ing", "ed", "able", "ible", "ment", "ness", "ly"];
 
 function checkPrefix(word) {
   for (const p of PREFIXES) {
@@ -26,9 +26,10 @@ export const getVocabForSurvey = async (req, res) => {
   try {
     const { number, offset} = req.body;
 
-    const rows = await collection.find({}, { projection: { _id: 0, vocab: 1 } }).skip(Number(offset)).limit(Number(number)).toArray();
+    const rows = await Dict.find({}).skip(Number(offset)).limit(Number(number))
+    
     const result = rows.map((row) => {
-      const word = row.vocab;
+      const word = row["vocab"];
       return {
         word,
         length_word: word.length,
